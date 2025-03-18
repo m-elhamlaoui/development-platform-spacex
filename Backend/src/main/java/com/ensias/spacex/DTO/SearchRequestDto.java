eafcd641240875c3c5dec29043b53b1c36f8af42
Thinkie <aaelbouziyani@gmail.com>
2025-03-18 14:13:35 +0000

    Revert "Feedback"

--- Backend/src/main/java/com/ensias/spacex/DTO/SearchRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.ensias.spacex.DTO;
-
-import lombok.Getter;
-
-@Getter
-public class SearchRequestDto {
-    private String depart;
-    private String arrive;
-}
--- settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-pluginManagement {
-	repositories {
-		maven { url = uri("https://repo.spring.io/snapshot") }
-		gradlePluginPortal()
-	}
-}
-rootProject.name = "SpaceX"
-include("Backend","Frontend")
