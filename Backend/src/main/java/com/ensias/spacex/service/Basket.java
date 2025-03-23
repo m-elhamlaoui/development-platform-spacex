a534ddc1c54380b251382ca9a097ec3bb37da5d6
Ahmed <Ahmedlahmadi469@gmail.com>
2025-03-23 19:52:46 +0000

    seesion-scoped for addToBasket & removeFromBasket api

--- Backend/src/main/java/com/ensias/spacex/service/Basket.java
+++ Backend/src/main/java/com/ensias/spacex/service/Basket.java
@@ -11,25 +11,17 @@ import java.util.List;
 @SessionScope
 public class Basket {
 
-    // Liste des voyages dans le panier
     private final List<Travel> travels = new ArrayList<>();
-
-
-     //recuperer les voyages du panier
 
     public List<Travel> getTravels() {
         return travels;
     }
 
 
-     // ajouter un voyage au panier
-
     public void addTravel(Travel travel) {
         travels.add(travel);
     }
 
-
-     // retirer du panier un voyage par son ID
 
     public void removeTravelById(Long travelId) {
         travels.removeIf(t -> t.getId().equals(travelId));
