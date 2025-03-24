e52d5df0fdbe927b4b0f483084cce5bd52fad4e2
nisrinba <nisrinebakhouch1@gmail.com>
2025-03-24 23:26:07 +0000

    refactored hardcoded planet values to use an enum

---
+++ Frontend/src/app/enums/planet.enum.ts
@@ -0,0 +1,9 @@
+export enum Planet {
+    Jupiter = 'jupiter',
+    Mars = 'mars',
+    Mercure = 'mercure',
+    Neptune = 'neptune',
+    Terre = 'terre',
+    Uranus = 'uranus',
+    Venus = 'venus'
+}
