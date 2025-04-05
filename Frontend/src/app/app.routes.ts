21bf10a86e7db00f904d323f7182d5d8fce859c6
wissalm1 <moutafatin2003@gmail.com>
2025-04-05 21:01:02 +0000

    .

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    {path :'recherche',loadComponent:()=> import('./components/search/search.component').then((c)=> c.SearchComponent)},
-    {path :'réservation',loadComponent:()=> import('./components/booking/booking.component').then((c)=> c.BookingComponent)},
-    {path :'liste_voyages',loadComponent:()=> import('./components/trip-list/trip-list.component').then((c)=> c.TripListComponent)},
+    { path: 'recherche', loadComponent: () => import('./components/search/search.component').then((c) => c.SearchComponent) },
+    { path: 'réservation', loadComponent: () => import('./components/booking/booking.component').then((c) => c.BookingComponent) },
+    { path: 'liste_voyages', loadComponent: () => import('./components/trip-list/trip-list.component').then((c) => c.TriplistComponent) },
 
 ];
