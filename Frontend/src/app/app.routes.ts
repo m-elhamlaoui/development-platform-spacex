dace7bc63eb822ee1aab4af7f27fb6c3b1ebfd53
wissalm1 <moutafatin2003@gmail.com>
2025-04-11 18:03:32 +0100

    fixing typos

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,6 +4,6 @@ export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'recherche', loadComponent: () => import('./components/search/search.component').then((c) => c.SearchComponent) },
     { path: 'réservation', loadComponent: () => import('./components/booking/booking.component').then((c) => c.BookingComponent) },
-    { path: 'liste_voyages', loadComponent: () => import('./components/trip-list/trip-list.component').then((c) => c.TriplistComponent) },
+    { path: 'liste_voyages', loadComponent: () => import('./components/trip-list/trip-list.component').then((c) => c.TripListComponent) },
 
 ];
--- Frontend/src/app/components/card/card.component.ts
+++ Frontend/src/app/components/card/card.component.ts
@@ -4,6 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { SearchReplyDto } from "../../DTO/searchReply.dto";
 
 @Component({
   selector: 'app-card',
@@ -25,10 +26,14 @@ export class CardComponent {
   @Input() departureDate: string = '';
   @Input() arrivalDate: string = '';
   @Input() travelId!: number;
+  @Input() travel!: SearchReplyDto;
+  @Input() mode: string = '';
+
   @Input() actionButtons: {
     icon: string;
     action: string;
     tooltip?: string;
+    color?: string;
   }[] = [];
 
   @Output() buttonClick = new EventEmitter<{ action: string; travelId: number }>();
