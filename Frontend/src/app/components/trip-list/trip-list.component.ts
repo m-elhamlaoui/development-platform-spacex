d6548f25bf18888dd94d505173e7bac05f94fdc8
nisrinba <nisrinebakhouch1@gmail.com>
2025-04-12 03:20:05 -0700

    fix erreur

--- Frontend/src/app/components/trip-list/trip-list.component.ts
+++ Frontend/src/app/components/trip-list/trip-list.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../card/card.component";
 
 @Component({
   selector: 'app-trip-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './trip-list.component.html',
   styleUrl: './trip-list.component.css'
 })
 export class TripListComponent {
+travels=[
+  {depart : 'hhhhhh',
+  arrive:'hhhhhh',
+  dateDepart:"hhhhh",
+  dateArrivee:"hhhhh",
+  prix:44
 
+  }
+]
 }
