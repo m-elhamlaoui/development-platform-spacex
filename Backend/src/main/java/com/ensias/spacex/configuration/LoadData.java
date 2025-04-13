8af764db705677da65c024865435aa710e762804
Ahmed <Ahmedlahmadi469@gmail.com>
2025-04-13 13:54:24 +0100

    adding two travels

--- Backend/src/main/java/com/ensias/spacex/configuration/LoadData.java
+++ Backend/src/main/java/com/ensias/spacex/configuration/LoadData.java
@@ -17,6 +17,10 @@ public class LoadData {
         return args -> {
             repo.save(new Travel("TRR","MRS",new Date(1715820393000L), new Date(1747836720000L),45000L)); // add a travel to travel list
             repo.save(new Travel("MRS","JUP",new Date(1745014392000L), new Date(1776550392L),45000L)); // add a travel to
+
+            repo.save(new Travel("JUP", "neptune", new Date(1746163200000L), new Date(1746422400000L), 60000L)); //: depart  2 mai 2025, arrivee  10 mai 2025
+            repo.save(new Travel("venus", "TRR", new Date(1746163200000L), new Date(1746854400000L), 52000L));
+
             repo.flush();
         };
     }
