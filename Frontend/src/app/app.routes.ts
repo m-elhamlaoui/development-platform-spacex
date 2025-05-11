c552e6c381b7de3634940a63064314473114bdfa
EL BOUZIYANI Anas <aaelbouziyani@gmail.com>
2025-05-11 03:04:36 +0100

    fix routing typo (Hotfix)

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '', redirectTo: '/recherche', pathMatch: 'full' }, // there is no home defined switched to search
     {path :'recherche',loadComponent:()=> import('./components/search/search.component').then((c)=> c.SearchComponent)},
     {path :'réservation',loadComponent:()=> import('./components/booking/booking.component').then((c)=> c.BookingComponent)},
     {path :'liste_voyages',loadComponent:()=> import('./components/trip-list/trip-list.component').then((c)=> c.TripListComponent)},
